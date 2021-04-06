efe2755740983261523844d7b7355ddee1360941
Alexander Smarus <bender@readdle.com>
2021-04-06 19:49:49 +0300

    rename resulting xcframework to unicode

--- create-xcframework.sh
+++ create-xcframework.sh
@@ -23,54 +23,61 @@ ICU_FRAMEWORK_VERSION=${ICU_VERSION_MAJOR}.${ICU_VERSION_MINOR}
 ICU_FRAMEWORK_COMPATIBILITY_VERSION=${ICU_VERSION_MAJOR}.0
 
 echo "-- Building macOS framework"
-xcodebuild clean archive \
- -project RDICU4c/RDICU4c.xcodeproj \
- -scheme RDICU4c-macOS \
+xcodebuild archive \
+ -project RDICU4c/unicode.xcodeproj \
+ -scheme unicode-macOS \
  -configuration Release \
  -arch arm64 \
  -arch x86_64 \
  -sdk macosx \
- -archivePath Archive/RDICU4c-macosx.xcarchive \
+ -archivePath Archive/unicode-macosx.xcarchive \
  DYLIB_COMPATIBILITY_VERSION=${ICU_FRAMEWORK_COMPATIBILITY_VERSION} DYLIB_CURRENT_VERSION=${ICU_FRAMEWORK_VERSION}
 
 echo "-- Building iOS framework"
-xcodebuild clean archive \
- -project RDICU4c/RDICU4c.xcodeproj \
- -scheme RDICU4c-iOS \
+xcodebuild archive \
+ -project RDICU4c/unicode.xcodeproj \
+ -scheme unicode-iOS \
  -configuration Release \
  -arch arm64 \
  -arch armv7 \
  -arch armv7s \
  -sdk iphoneos \
- -archivePath Archive/RDICU4c-iphoneos.xcarchive \
+ -archivePath Archive/unicode-iphoneos.xcarchive \
  DYLIB_COMPATIBILITY_VERSION=${ICU_FRAMEWORK_COMPATIBILITY_VERSION} DYLIB_CURRENT_VERSION=${ICU_FRAMEWORK_VERSION}
 
 echo "-- Building iOS Simulator framework"
-xcodebuild clean archive \
- -project RDICU4c/RDICU4c.xcodeproj \
- -scheme RDICU4c-iOS \
+xcodebuild archive \
+ -project RDICU4c/unicode.xcodeproj \
+ -scheme unicode-iOS \
  -configuration Release \
  -arch arm64 \
  -arch x86_64 \
  -sdk iphonesimulator \
- -archivePath Archive/RDICU4c-iphonesimulator.xcarchive \
+ -archivePath Archive/unicode-iphonesimulator.xcarchive \
  DYLIB_COMPATIBILITY_VERSION=${ICU_FRAMEWORK_COMPATIBILITY_VERSION} DYLIB_CURRENT_VERSION=${ICU_FRAMEWORK_VERSION}
 
 echo "-- Building Mac Catalyst framework"
-xcodebuild clean archive \
- -project RDICU4c/RDICU4c.xcodeproj \
- -scheme RDICU4c-iOS \
+xcodebuild archive \
+ -project RDICU4c/unicode.xcodeproj \
+ -scheme unicode-iOS \
  -configuration Release \
  -arch x86_64 \
  -arch arm64 \
  -sdk macosx \
- -archivePath Archive/RDICU4c-maccatalyst.xcarchive \
+ -archivePath Archive/unicode-maccatalyst.xcarchive \
  SUPPORTS_MACCATALYST=YES DYLIB_COMPATIBILITY_VERSION=${ICU_FRAMEWORK_COMPATIBILITY_VERSION} DYLIB_CURRENT_VERSION=${ICU_FRAMEWORK_VERSION}
 
 echo "-- Creating XCFramework..."
 xcodebuild -create-xcframework \
- -framework "Archive/RDICU4c-macosx.xcarchive/Products/Library/Frameworks/RDICU4c.framework" \
- -framework "Archive/RDICU4c-iphoneos.xcarchive/Products/Library/Frameworks/RDICU4c.framework" \
- -framework "Archive/RDICU4c-iphonesimulator.xcarchive/Products/Library/Frameworks/RDICU4c.framework" \
- -framework "Archive/RDICU4c-maccatalyst.xcarchive/Products/Library/Frameworks/RDICU4c.framework" \
- -output "XCFramework/RDICU4c.xcframework"
+ -framework "Archive/unicode-macosx.xcarchive/Products/Library/Frameworks/unicode.framework" \
+ -framework "Archive/unicode-iphoneos.xcarchive/Products/Library/Frameworks/unicode.framework" \
+ -framework "Archive/unicode-iphonesimulator.xcarchive/Products/Library/Frameworks/unicode.framework" \
+ -framework "Archive/unicode-maccatalyst.xcarchive/Products/Library/Frameworks/unicode.framework" \
+ -output "XCFramework/unicode.xcframework"
+
+echo "-- Compressing XCFramework..."
+cd XCFramework
+zip -ry RDICU4c.zip unicode.xcframework 
+cd ..
+
+echo "-- Done"
